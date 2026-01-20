73a51877a12eeadfb6052a28fb7004bb5f92c49c
Dmitry <officialforworkandstudy@gmail.com>
2026-01-20 14:37:04 +0300

    WebConfig.java changed (transfer control to Tomcat)

--- backend-app/src/main/java/config/WebConfig.java
+++ backend-app/src/main/java/config/WebConfig.java
@@ -2,6 +2,7 @@ package config; // или com.example.config
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,4 +11,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan("controller") // <--- Говорим искать Контроллеры в папке controller
 public class WebConfig implements WebMvcConfigurer {
     // Здесь можно настраивать всякие штуки типа CORS, но пока оставим пустым
+	
+	@Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
